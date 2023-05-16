cc9090f27dbcbe4b342c8669289b3f94460e03d9
betazetadev <hi@betazetadev.com>
2023-05-16 18:25:38 +0200

    Padding in the dialog header title widget

--- lib/widget/details_header_widget.dart
+++ lib/widget/details_header_widget.dart
@@ -1,24 +1,26 @@
 import 'package:flutter/material.dart';
 
 Widget detailsHeaderWidget(BuildContext context, String title) {
-  return Row(
-    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-    mainAxisSize: MainAxisSize.min,
-    children: [
-      Expanded(
-        child: Text(
-          title,
-          style: Theme.of(context).textTheme.headlineSmall!.copyWith(
-                color: Theme.of(context).primaryColor,
-                fontWeight: FontWeight.bold,
-              ),
-          textAlign: TextAlign.center,
-        ),
-      ),
-      IconButton(
-        onPressed: () => Navigator.of(context).pop(),
-        icon: const Icon(Icons.close),
-      ),
-    ],
-  );
+  return Padding(
+      padding: const EdgeInsets.only(top: 16, bottom: 8),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Expanded(
+            child: Text(
+              title,
+              style: Theme.of(context).textTheme.headlineSmall!.copyWith(
+                    color: Theme.of(context).primaryColor,
+                    fontWeight: FontWeight.bold,
+                  ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          IconButton(
+            onPressed: () => Navigator.of(context).pop(),
+            icon: const Icon(Icons.close),
+          ),
+        ],
+      ));
 }
