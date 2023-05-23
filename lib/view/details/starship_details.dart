8f9abc28bb520353c24fbb14db71637acd08beea
betazetadev <hi@betazetadev.com>
2023-05-23 19:20:51 +0200

    Translated the texts from the starship details dialog

--- lib/view/details/starship_details.dart
+++ lib/view/details/starship_details.dart
@@ -4,6 +4,7 @@ import '../../model/starship.dart';
 import '../../widget/details_header_widget.dart';
 import '../../widget/details_row_widget.dart';
 import '../../widget/details_subheader_widget.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class StarshipDetailsDialog extends StatelessWidget {
   final Starship starship;
@@ -22,22 +23,55 @@ class StarshipDetailsDialog extends StatelessWidget {
           children: [
             detailsHeaderWidget(context, starship.name),
             detailsSubheaderWidget(context, starship.model),
-            detailsRowWidget(context, "Class", starship.starshipClass),
             detailsRowWidget(
-                context, "Cost", "${starship.costInCredits} Galactic Credits"),
-            detailsRowWidget(context, "Length", "${starship.length} meters"),
+                context,
+                AppLocalizations.of(context)?.starship_class ??
+                    AppLocalizations.of(context)!.not_available,
+                starship.starshipClass),
             detailsRowWidget(
-                context, "Cargo capacity", "${starship.cargoCapacity} kg"),
-            detailsRowWidget(context, "Consumables", starship.consumables),
-            detailsRowWidget(context, "Crew", starship.crew),
-            detailsRowWidget(context, "Passengers", starship.passengers),
-            detailsRowWidget(context, "Max atmosphering speed",
-                starship.maxAtmospheringSpeed?.toString() ?? "n/a"),
+                context,
+                AppLocalizations.of(context)?.cost ??
+                    AppLocalizations.of(context)!.not_available,
+                AppLocalizations.of(context)
+                        ?.starship_cost_text(starship.costInCredits) ??
+                    AppLocalizations.of(context)!.not_available),
+            detailsRowWidget(
+                context,
+                AppLocalizations.of(context)?.length ??
+                    AppLocalizations.of(context)!.not_available,
+                "${starship.length}m"),
+            detailsRowWidget(
+                context,
+                AppLocalizations.of(context)?.cargo_capacity ??
+                    AppLocalizations.of(context)!.not_available,
+                "${starship.cargoCapacity}kg"),
+            detailsRowWidget(
+                context,
+                AppLocalizations.of(context)?.consumables ??
+                    AppLocalizations.of(context)!.not_available,
+                starship.consumables),
+            detailsRowWidget(
+                context,
+                AppLocalizations.of(context)?.crew ??
+                    AppLocalizations.of(context)!.not_available,
+                starship.crew),
+            detailsRowWidget(
+                context,
+                AppLocalizations.of(context)?.passengers ??
+                    AppLocalizations.of(context)!.not_available,
+                starship.passengers),
+            detailsRowWidget(
+                context,
+                AppLocalizations.of(context)?.max_atmosphering_speed ??
+                    AppLocalizations.of(context)!.not_available,
+                starship.maxAtmospheringSpeed?.toString() ??
+                    AppLocalizations.of(context)!.not_available),
             detailsRowWidget(context, "MGLT", starship.mglt.toString()),
             chipListWidget(
                 context: context,
                 items: starship.manufacturers,
-                title: "Manufacturers"),
+                title: AppLocalizations.of(context)?.manufacturers ??
+                    AppLocalizations.of(context)!.not_available),
             const SizedBox(height: 16),
           ],
         ),
